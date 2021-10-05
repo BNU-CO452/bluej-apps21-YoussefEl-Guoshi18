69df6e2324b123b2ef70d2f73fa1166922dca20e
YoussefEl-Guoshi18 <elguoshis@gmail.com>
2021-10-05 11:55:59 +0100

    Module app

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,53 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author Youssef El-Guoshi
+ * @version 05/10/2021
+ */
+public class Module
+{
+    // instance variables - replace the example below with your own
+    private String code;
+    private String title;
+    private Module module;
+    private int credit;
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module(String code, String title)
+    {
+       this.code = code;
+       this.title = title;
+       credit = 0;
+    }
+
+    public String getCode()
+    {
+        return code;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    public int getCredit()
+    {
+        
+        return credit;
+    }
+    
+    public void setCredit(int credit)
+    {
+        this.credit = credit;
+    }
+    
+    public void print()
+    {
+        //printHeading();
+        
+        System.out.println(" Module Code: " + code + ": " + title);
+        System.out.println();
+    }
+}
